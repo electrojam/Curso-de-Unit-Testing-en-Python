0e3346d7be5f11c47f02d96318a005aa153482ed
electrojam <electrojam@hotmail.com>
2024-09-09 06:56:40 +0000

    RETO Instalación y Configuración del Entorno de Pruebas3 / 2

--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,9 @@ def sum(a, b):
 
 def substract(a, b):
     return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
--- tests/tests_calculator.py
+++ tests/tests_calculator.py
@@ -1,11 +1,21 @@
 import unittest
 
-from src.calculator import sum, substract
+from src.calculator import sum, substract, multiplication, division
 
 class CalculatorTests(unittest.TestCase):
 
-    def test_sum(self):
-        assert sum(2 ,3) == 6
+    def test_sum(self):     #Test suma
+        assert sum(2, 3) == 5
 
-    def test_substract(self):
-        assert substract(10 ,5) == 5
+    def test_substract(self):   # Test resta
+        assert substract(10, 5) == 5
+    
+    def test_multiplication(self):  # Test multiplicación
+        assert multiplication(10, 5) == 50
+    
+    def test_division(self):    # Test división
+        assert division(10, 5) == 2
+    
+    def test_division_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            division(30, 0)
