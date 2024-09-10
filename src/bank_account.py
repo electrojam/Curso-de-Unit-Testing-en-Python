e57018bb034ff05511cdfb997266733be937d45a
electrojam <electrojam@hotmail.com>
2024-09-10 00:01:36 +0000

    Uso de tearDown para limpieza de Pruebas Unitarias en Python6 / 20

--- src/bank_account.py
+++ src/bank_account.py
@@ -1,17 +1,27 @@
 class BankAccount:
-    def __init__(self, balance=0):
+    def __init__(self, balance=0, log_file=None):
         self.balance = balance
+        self.log_file = log_file
+        self.log_transaction('Cuenta creada')
+        
+    def log_transaction(self, message):
+        if self.log_file:
+            with open(self.log_file, mode='a') as f:
+                f.write(f"{message}\n")
 
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.log_transaction(f"Deposited {amount}.  New balance is: {self.balance}")
         return self.balance
 
     def withdraw(self, amount):
         if amount > 0:
             self.balance -= amount
+            self.log_transaction(f"Withdrawed {amount}.  New balance is: {self.balance}")
         return self.balance
     
     def get_balance(self):
+        self.log_transaction(f"Checked balance.  Currente balance is: {self.balance}")
         return self.balance
     
--- tests/tests_bank_account.py
+++ tests/tests_bank_account.py
@@ -1,11 +1,19 @@
-import unittest
+import unittest, os
 
 from src.bank_account import BankAccount
 
 class BankAccountTets(unittest.TestCase):   # Clase con métodos de prueba
 
-    def setUp(self):
-        self.account = BankAccount(balance = 1000)
+    def setUp(self):    # método a ejecutar antes de cada prueba
+        self.account = BankAccount(balance = 1000, log_file="log_transaction.txt")
+
+    def tearDown(self): # método a ejecutar despúes de cada prueba
+        if os.path.exists(self.account.log_file):   # miramos si archivo log existe
+            os.remove(self.account.log_file)
+
+    def count_lines(self, filename):    # Método para contar líneas del log (este método no es prueba)
+        with open(filename, "r") as f:
+            return len(f.readlines())
 
     def test_deposit(self): # prueba depósito
         new_balance = self.account.deposit(500)
@@ -17,3 +25,12 @@ class BankAccountTets(unittest.TestCase):   # Clase con métodos de prueba
 
     def test_get_balance(self): # prueba para obtener balance
         assert self.account.get_balance() == 1000
+
+    def test_log_transaction(self): # Prueba de creación del log
+        self.account.deposit(500)
+        assert os.path.exists(self.account.log_file)    # Miramos si el archivo log existe
+
+    def test_count_transactions(self):  # Probamos método count_lines
+        assert self.count_lines(self.account.log_file) == 1
+        self.account.deposit(500)
+        assert self.count_lines(self.account.log_file) == 2
