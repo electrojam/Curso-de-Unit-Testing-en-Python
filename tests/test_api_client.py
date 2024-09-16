eae9d73e4ef3c8bad71c83c723ee4ee943280146
electrojam <electrojam@hotmail.com>
2024-09-16 03:48:12 +0000

    Uso de Side Effects en Mocking con Python12 / 20

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -28,24 +28,24 @@ class ApiClientTests(unittest.TestCase):
     @patch('src.api_client.requests.get')   # vamos a sobreescribir la línea método requests.get de api_client.py
     def test_get_location_returns_side_effect(self, mock_get):  # prueba de función get_location
         mock_get.side_effect = [
-            requests.exceptions.RequestException("Service unavailable")
+            requests.exceptions.RequestException("Service unavailable"),
+            unittest.mock.Mock(
+                status_code=200,
+                json = lambda: {
+                    "countryName": "USA",
+                    "regionName": "FLORIDA",
+                    "cityName": "MIAMI",
+                    "countryCode": "US"
+                }
+            )
         ]
-        mock_get.return_value.status_code = 200 #cuando se consulte requests.get retornarrá status code = 200        
-        mock_get.return_value.json.return_value = { #cuando ejecute requests.get retornará este json
-            "countryName": "USA",
-            "regionName": "FLORIDA",
-            "cityName": "MIAMI",
-            "countryCode": "US"
-        }
-        with self.assertRaises(
-            requests.exceptions.RequestException):    # capturamos excepcioón del requests.get()
+        with self.assertRaises(requests.exceptions.RequestException):    # capturamos excepcioón del requests.get()
             get_location("8.8.8.8")
-            
         
-        # result = get_location("8.8.8.8")
-        # self.assertEqual(result.get("country"), "USA")
-        # self.assertEqual(result.get("region"), "FLORIDA")
-        # self.assertEqual(result.get("city"), "MIAMI")
-        # self.assertEqual(result.get("code"), "US")
+        result = get_location("8.8.8.8")
+        self.assertEqual(result.get("country"), "USA")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
+        self.assertEqual(result.get("code"), "US")
 
         
