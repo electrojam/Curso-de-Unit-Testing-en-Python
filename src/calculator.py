8355519ca317ad02b213b6646a92dfd92d8461f6
electrojam <electrojam@hotmail.com>
2024-09-17 01:21:24 +0000

    Documentación de pruebas unitarias con Doctest en Python15 / 20

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,11 @@
 def sum(a, b):
+    """
+    >>> sum(5, 7)
+    12
+
+    >>> sum(4, -4)
+    0
+    """
     return a + b
 
 def substract(a, b):
@@ -8,4 +15,12 @@ def multiplication(a, b):
     return a * b
 
 def division(a, b):
+    """
+    >>> division(10, 0)
+    Traceback (most recent call last):
+    ValueError: La división por cero no está permitida
+    """
+
+    if b == 0:
+        raise ValueError("La división por cero no está permitida")
     return a / b
